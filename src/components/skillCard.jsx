be605301321097a44f61f3ab56f340cad252d6b1
Julio <ja.samaniegom@gmail.com>
2022-08-30 23:08:56 -0500

    skills with progress bar

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -7,8 +7,15 @@ const SkillCard = ({skill}) => {
         <img src={skill.logo} alt={skill.name}/>
       </div>
       <div className='skill-nameAndLevel'>
-        <h3>{skill.name}</h3>
+        <h4>{skill.name}</h4>
         <p>Level: {skill.level}</p>
+      </div>
+      <div className='progres-bar-container'>
+        <div className='progres-bar' style={{width :`${(skill.percent)}%`}}></div>
+        <div className='cero-100'>
+          <div>0</div>
+          <div>100</div>
+        </div>
       </div>
     </div>
   )
